7772bb5a4c3f289818502c0d78e69cd9791d63dc
João Canabarro Campos <joaocampos963@icloud.com>
2025-04-07 11:09:05 +0200

    Small fix on Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer: React.FC = () => {
         <footer className="footer">
             <p className="font-oxygen text-center text-sm text-gray-500">Created by João, Mahmoud, Nikita & Pinar </p>
             <p className="text-center text-sm text-gray-500">© {new Date().getFullYear()} Local Travel & Weather Dashboard. All rights reserved.</p>
-            <p className="text-center text-sm text-gray-500">Powered by OpenWeatherMap API,Google Maps API, OpenWeather API, SunriseSunsteAPI & TrafikverketsAPI.</p>
+            <p className="text-center text-sm text-gray-500">Powered by GoogleMaps API, OpenWeatherAPI, Sunrise&SunsetAPI, ResrobotAPI & TrafikverketsAPI.</p>
 
         </footer>
     );
